7422843e97f058f8e73d9d902992b17d47a97cd1
Waseem Sajjad <waseemsajjadkcb@gmail.com>
2023-10-04 17:37:39 +0500

    Updates

---
+++ assets/Button-31c035a1.js
@@ -0,0 +1,13 @@
+import{j as n,a as t,F as s,B as e}from"./Button.stories-7d6ade83.js";import{M as c,P as l,C as f,b as i,d as o}from"./index-d2f0bd56.js";import{u as m}from"./index-a1cf9e47.js";import"./index-76fb7be0.js";import"./_commonjsHelpers-de833af9.js";import"./iframe-0840196b.js";import"../sb-preview/runtime.js";import"./index-ffc7e5ff.js";import"./index-d37d4223.js";import"./index-356e4a49.js";function d(r){const a=Object.assign({h1:"h1",h2:"h2",strong:"strong"},m(),r.components);return t(s,{children:[n(c,{of:e}),`
+`,n(a.h1,{id:"button",children:"Button"}),`
+`,n(l,{}),`
+`,n(f,{}),`
+`,n(a.h2,{id:"appearance",children:n(a.strong,{children:"Appearance"})}),`
+`,t(i,{children:[n(o,{of:e,name:"Primary"}),n(o,{of:e,name:"Danger"}),n(o,{of:e,name:"Ghost"}),n(o,{of:e,name:"Link"})]}),`
+`,n(a.h2,{id:"rounded",children:n(a.strong,{children:"Rounded"})}),`
+`,t(i,{children:[n(o,{of:e,name:"Rounded - Default"}),n(o,{of:e,name:"Rounded - None"}),n(o,{of:e,name:"Rounded - Small"}),n(o,{of:e,name:"Rounded - Medium"}),n(o,{of:e,name:"Rounded - Large"}),n(o,{of:e,name:"Rounded - Full"})]}),`
+`,n(a.h2,{id:"size",children:n(a.strong,{children:"Size"})}),`
+`,t(i,{children:[n(o,{of:e,name:"Size - Large"}),n(o,{of:e,name:"Size - Medium"}),n(o,{of:e,name:"Size - Default"}),n(o,{of:e,name:"Size - Small"})]}),`
+`,n(a.h2,{id:"loader-state",children:n(a.strong,{children:"Loader State"})}),`
+`,n(i,{children:n(o,{of:e,name:"Loader - Default"})})]})}function C(r={}){const{wrapper:a}=Object.assign({},m(),r.components);return a?n(a,Object.assign({},r,{children:n(d,r)})):d(r)}export{C as default};
+//# sourceMappingURL=Button-31c035a1.js.map
