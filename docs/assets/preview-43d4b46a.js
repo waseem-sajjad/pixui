bc0b14774c181ad50abf9354096bf4e2a6c8bc45
Waseem Sajjad <waseemsajjadkcb@gmail.com>
2023-10-24 10:50:28 +0500

    add -D eslint-plugin-react-hooks

---
+++ docs/assets/preview-43d4b46a.js
@@ -0,0 +1,2 @@
+import{_ as e}from"./iframe-7b75a549.js";import"../sb-preview/runtime.js";var a={docs:{renderer:async()=>{let{DocsRenderer:r}=await e(()=>import("./DocsRenderer-NNNQARDV-17fbd3cc.js"),["./DocsRenderer-NNNQARDV-17fbd3cc.js","./iframe-7b75a549.js","./index-76fb7be0.js","./_commonjsHelpers-de833af9.js","./react-18-063a39db.js","./index-932496f1.js","./index-c3ecc91d.js","./index-d37d4223.js","./index-356e4a49.js"],import.meta.url);return new r}}};export{a as parameters};
+//# sourceMappingURL=preview-43d4b46a.js.map
